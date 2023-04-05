2ee0f6b181e254e80824553debcee45ce972b39e
bbhitec <bboy.hitec@gmail.com>
2023-04-05 19:43:49 +0300

    lint optimizations

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
             <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Front End Developer</h3>
             {/* Limit the paragraph width */}
             <p className='relative text-md py-5 leading-8 text-gray-800 md:text-lg max-w-xl mx-auto dark:text-gray-200'>
-              Freelancer providing services for programming and design content needs. Join me down below and let's get cracking!
+              Freelancer providing services for programming and design content needs. Join me down below and let&apos;s get cracking!
             </p>
           </div>
 
@@ -72,7 +72,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-2 dark:text-white'>Services I Offer</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              Since the beginning of my journey as a freelance designer and developer, I've done remote work for
+              Since the beginning of my journey as a freelance designer and developer, I&apos;ve done remote work for
               <span className='text-purple-500'> agencies </span> consulted for <span className='text-purple-500'>startups </span>
               and collaborated with talanted people to create digital products for both business and consumer use.
             </p>
@@ -100,7 +100,7 @@ export default function Home() {
               <Image src={code} width={100} height={100} className='mx-auto' />
               <h3 className='text-lg font-medium pt-8 pb-2'>Code your dream project</h3>
               <p className='py-2'>
-                Do you have an idea for your next great website? Let's make it a reality.
+                Do you have an idea for your next great website? Let&apos;s make it a reality.
               </p>
               <h4 className='py-4 text-purple-600'>Design Tools I Use</h4>
               <p className='text-gray-800 py-1'>Photoshop</p>
@@ -128,7 +128,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-1 dark:text-white '>Portofolio</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              Since the beginning of my journey as a freelance designer and developer, I've done remote work for
+              Since the beginning of my journey as a freelance designer and developer, I&apos;ve done remote work for
               <span className='text-purple-500'> agencies </span> consulted for <span className='text-purple-500'>startups </span>
               and collaborated with talanted people to create digital products for both business and consumer use.
             </p>
